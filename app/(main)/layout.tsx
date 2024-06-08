5f56e1022f6bc3ec3b9a7077fee599594609490e
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2024-06-08 23:19:57 +0600

    feat: navbar done

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,29 @@
+"use client";
+import Spinner from "@/components/common/Spinner";
+import Navigation from "@/components/MainComp/Navigation";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import { PropsWithChildren } from "react";
+
+export default function MainLayout({ children }: PropsWithChildren) {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size={"lg"} />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full flex dark:bg-[#1f1f1f]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">{children}</main>
+    </div>
+  );
+}
